9149c948d25019a6e94d19275250e86a83271763
deeph0205 <76654650+deeph0205@users.noreply.github.com>
2022-03-18 19:48:50 -0400

    Update Project.sol
    This implementation allows a user to vote on a number of polls multiple times then displays the correct winning choice for that poll.

--- Project.sol
+++ Project.sol
@@ -4,10 +4,11 @@ pragma solidity ^0.7.0;
 contract Polling_System {
 
     uint256 public number_of_polls = 0;
+    uint256 public number_of_users = 0;
     address public owner;
 
-    mapping(address => User) public Users;
-    mapping(uint => Poll) public List_of_Polls;
+    mapping(uint256 => User) public Users;
+    mapping(uint256 => Poll) public List_of_Polls;
     
     struct Poll {
         uint256 set_time;
@@ -28,6 +29,10 @@ contract Polling_System {
 
     constructor() {
         owner = msg.sender;
+        Users[number_of_users + 1].voted = false;
+        Users[number_of_users + 1].delegate = owner;
+        Users[number_of_users + 1].vote = 100;
+        number_of_users +=1;
     }
 
     function createPoll(uint256 _set_time, string memory _question, string memory _choice1,  string memory _choice2,  string memory _choice3 ) public {
@@ -40,5 +45,40 @@ contract Polling_System {
         List_of_Polls[number_of_polls + 1].choice3 = _choice3;
         List_of_Polls[number_of_polls + 1].choice3_count = 0;
         number_of_polls +=1;
+    }
+
+    function addVote(uint256 _select_poll, uint256 _select_choice) public {
+        require(_select_choice <= 3, "Please choose a choice between numbers 1 and 3");
+        require(_select_poll <= number_of_polls, "Please choose a correct Poll number to vote");
+        if(_select_choice == 1) {
+            List_of_Polls[_select_poll].choice1_count += 1;
+        }
+        if(_select_choice == 2) {
+            List_of_Polls[_select_poll].choice2_count += 1;
+        }
+        if(_select_choice == 3) {
+            List_of_Polls[_select_poll].choice3_count += 1;
+        }
+
+    }
+
+    function displayWinner(uint256 _select_poll) public view returns (string memory)  {
+        require(_select_poll <= number_of_polls, "Please choose a correct Poll number to Display winner");
+        uint256 max_count = 0;
+        string memory max_string;
+        if(List_of_Polls[_select_poll].choice1_count > max_count) {
+            max_count = List_of_Polls[_select_poll].choice1_count;
+            max_string = List_of_Polls[_select_poll].choice1;
+        }
+        if(List_of_Polls[_select_poll].choice2_count > max_count) {
+            max_count = List_of_Polls[_select_poll].choice2_count;
+            max_string = List_of_Polls[_select_poll].choice2;
+        }
+        if(List_of_Polls[_select_poll].choice3_count > max_count) {
+            max_count = List_of_Polls[_select_poll].choice3_count;
+            max_string = List_of_Polls[_select_poll].choice3;
+        }
+        return max_string;
+
     }
 }
